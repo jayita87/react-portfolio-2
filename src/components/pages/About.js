62d3e535e4f3611beaa0f3d22b29a4ad8568b333
Jayita Chowdhury <jayitachowdhury@Amits-MacBook-Pro.local>
2022-11-30 16:41:42 +0530

    change about.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,25 +5,24 @@ import Footer from '../Footer';
 import portrait from '../../assets/portrait.jpeg';
 
 export default function About() {
-    return (
-      <div>
-        <Header />
-        
-        <div className="content">
-          <h2 id="h2">About me</h2>
-          <img className="image" src={portrait} alt="portrait" width="150"></img>
-          <div className="bio">
+  return (
+    <div>
+      <Header />
+
+      <div className="content">
+        <h2 id="h2">About me</h2>
+        <img className="image" src={portrait} alt="portrait" width="150"></img>
+        <div className="bio">
           <p>
-          My name is Jayita Chowdhury. I live in Phoenix, Arizona with my family.I love to cook food and travel all around the world.
+            My name is Jayita Chowdhury. I live in Phoenix, Arizona with my family.I love to cook food and travel all around the world.
           I love to learn new things. I am studying full stack web developer course from university of Arizona
-          coding bootcamp. During the course we are learning creating beatiful, usable, professional websites using best practice accessibility and
-          the latest W3 web standard guidelines resulting in semantic and seo friendly HTML and CSS.
-          I design websites that convert visitors into customers and help your business grow. Given links to some of my websites to
-          showcase my potential to fit for a given position. All the Websites linked below are designed, built
-          according to latest web standards.
-          <br/>
-         </p>
-         </div>
+          coding bootcamp. I am a hard-working, detail-oriented Software Developer passionate about learning,
+          researching, and writing code and I relish the opportunity to take my programming skills and
+           experience to a company where
+           I can continue to learn and grow as a professional software developer.
+          <br />
+          </p>
+        </div>
       </div>
       <Footer />
     </div>
